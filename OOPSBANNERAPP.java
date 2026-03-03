9ac3f055864ebab1c719a7956b8a0f0bdd74eac1
as3708-crypto <as3708@srmist.edu.in>
2026-03-03 10:48:09 +0530

    Update OOPSBANNERAPP.java

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,8 +1,69 @@
-public class OOPSBannerApp
-{
- public static void main (String[] args)
- {
-     
+import java.util.HashMap;
+import java.util.Map;
+
+public class BannerUC8 {
+
+    public static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                "  OOOOO  ",
+                " OO   OO ",
+                " OO   OO ",
+                " OO   OO ",
+                " OO   OO ",
+                " OO   OO ",
+                "  OOOOO  "
+        });
+
+        patternMap.put('P', new String[]{
+                " PPPPPP  ",
+                " PP   PP ",
+                " PP   PP ",
+                " PPPPPP  ",
+                " PP      ",
+                " PP      ",
+                " PP      "
+        });
+
+        patternMap.put('S', new String[]{
+                "  SSSSS  ",
+                " SS   SS ",
+                " SS      ",
+                "  SSSSS  ",
+                "      SS ",
+                " SS   SS ",
+                "  SSSSS  "
+        });
+
+        return patternMap;
+    }
+
+    public static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        for (int row = 0; row < 7; row++) { 
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : word.toCharArray()) { 
+                String[] pattern = patternMap.get(ch);
+                if (pattern != null) {
+                    sb.append(pattern[row]).append("   ");
+                }
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        String word = "OOPS";
+
+        renderBanner(word, patternMap);
     }
 }
 
