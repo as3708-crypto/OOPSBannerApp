a7620baf65621302006ad165716225f25fb69094
as3708-crypto <as3708@srmist.edu.in>
2026-03-03 09:57:46 +0530

    Update OOPSBANNERAPP.java

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,8 +1,10 @@
-public class OOPSBannerApp
-{
- public static void main (String[] args)
- {
-     
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println(" ***   ***   ***   ***** ");
+        System.out.println("*   * *   * *   *  *     ");
+        System.out.println("*   * *   * *   *  ***** ");
+        System.out.println("*   * *   * *          * ");
+        System.out.println(" ***   ***  *      ***** ");
     }
 }
 
